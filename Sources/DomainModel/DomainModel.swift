7b1289758c5caeeae72a4ee063cdb5aec6ce5b2e
George Lee <george10lee@gmail.com>
2025-04-13 03:05:28 -0700

    debug moneyTest

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -8,7 +8,83 @@ struct DomainModel {
 ////////////////////////////////////
 // Money
 //
+import Foundation
+
+
+
 public struct Money {
+    let amount : Int
+    let currency: String
+    
+    init?(amount : Int, currency : String) {
+        if currency != "USD" && currency != "EUR" && currency != "GBP" && currency != "CAN" {
+            return nil;
+        } else {
+            self.amount = amount;
+            self.currency = currency;
+        }
+    }
+    
+    
+    func convert (toCur : String) -> Money {
+        var newCur : String
+        var newAmt : Double
+        
+        var toUSDAmt: Double = Double(self.amount);
+        
+        // Convert to USD
+        if self.currency == "EUR" {
+            toUSDAmt = toUSDAmt * (1.5/2)
+        }
+        else if self.currency == "GBP" {
+            toUSDAmt = toUSDAmt * (0.5/2)
+        }
+        else if self.currency == "CAN" {
+            toUSDAmt = toUSDAmt * (1.25/4)
+        }
+        else{
+            newCur = self.currency
+        }
+        
+        // Convert to new Cur
+        if toCur == "EUR" {
+            newAmt = toUSDAmt * (1.5/2)
+        }
+        else if toCur == "GBP" {
+            newAmt = toUSDAmt * (0.5/2)
+        }
+        else if toCur == "CAN" {
+            newAmt = toUSDAmt * (1.25/4)
+        }
+        else{
+            newAmt = toUSDAmt;
+        }
+        
+        newCur = toCur;
+        return Money(amount: Int(round(newAmt)), currency: newCur)!
+    }
+    
+    
+    
+  func  add(_ other: Money) -> Money {
+        if(self.currency != other.currency) {
+            var moneyConverted : Money = self.convert(toCur: other.currency)
+            return Money(amount: other.amount + moneyConverted.amount, currency: other.currency)!
+        }
+        else {
+            return Money(amount: self.amount + other.amount, currency: self.currency)!
+        }
+    }
+    
+   func  subtract(_ other:Money) -> Money {
+        if(self.currency != other.currency) {
+            var moneyConverted : Money = self.convert(toCur: other.currency)
+            return Money(amount: self.amount - moneyConverted.amount, currency: self.currency)!
+        }
+        else {
+            return Money(amount: self.amount - other.amount, currency: self.currency)!
+        }
+    }
 }
 
 ////////////////////////////////////
